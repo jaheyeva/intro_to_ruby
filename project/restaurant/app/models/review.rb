1d00fa92a7642847810d9858933e5b716bd97607
jaheyeva <jaheyeva@gmail.com>
2013-08-14 15:49:45 -0500

    Resolved  mass-assign protected attributes error

--- project/restaurant/app/models/review.rb
+++ project/restaurant/app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+	attr_accessible :title, :poster, :date, :article #added on August 14 to resolve Can't mass-assign protected attributes error
   validates :title, :presence => true
   validates :poster, :presence => true
 end
