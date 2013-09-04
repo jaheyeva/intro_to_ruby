aaebb3573c35942130ae13fdf5e0b42f81043191
jaheyeva <jaheyeva@gmail.com>
2013-09-04 11:20:28 -0500

    tests

--- project/restaurant/test/unit/review_test.rb
+++ project/restaurant/test/unit/review_test.rb
@@ -4,4 +4,9 @@ class ReviewTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "empty review" do
+  review = Review.new
+  assert !review.save    
+end  
 end
